6005b1b400873882c3875497375ef864846d18ad
Anya M <anya-aurore.mauron@epfl.ch>
2022-04-22 18:50:55 +0200

    project 1

--- Project1/project1_1.py
+++ Project1/project1_1.py
@@ -23,15 +23,16 @@ class Dataset(torch.utils.data.Dataset):
         'Initialization'
         if train: 
             x, y = torch.load("train_data.pkl")
-            print("Training data : \n noisiy_imgs_1 : ", x.shape, "\n noisy_imgs_2 : ", y.shape)
+            print("Training data : \n noisy_imgs_1 : ", x.shape, "\n noisy_imgs_2 : ", y.shape)
+            print("Type : ", x.dtype)
         else : 
             x, y = torch.load("val_data.pkl")
-            print("Test data : \n noisiy_imgs : ", x.shape, "\n clean_imgs : ", y.shape)
+            print("Test data : \n noisy_imgs : ", x.shape, "\n clean_imgs : ", y.shape)
         x, y = x[:SIZE], y[:SIZE]
-        print("Data reduced : \n noisiy_imgs_1_reduced : ", x.shape, "\n noisy_imgs_2_reduced : ", y.shape)
+        print("Data reduced : \n noisy_imgs_1_reduced : ", x.shape, "\n noisy_imgs_2_reduced : ", y.shape)
 
-        self.x = x
-        self.y = y
+        self.x = x.float()
+        self.y = y.float()
 
   def __len__(self):
         'Denotes the total number of samples'
@@ -44,18 +45,20 @@ class Dataset(torch.utils.data.Dataset):
         Y = self.y[index]
         return X, Y
 
-SIZE = 50
+SIZE = 500
+BATCH_SIZE = 5
 train_set = Dataset(SIZE)
-N = 10
+N = 20
 plt.figure()
 
-
+"""
 for i in range(N):
     plt.subplot(2, N, 2*i+1)
     plt.imshow(train_set.x[i].permute(1, 2, 0))
     plt.subplot(2, N, 2*i+2)
     plt.imshow(train_set.y[i].permute(1, 2, 0))
 plt.show()
+"""
 
 #let's create a simple auto-encoder first:
 class AE(torch.nn.Module):
@@ -63,11 +66,11 @@ class AE(torch.nn.Module):
         super().__init__()
 
         self.encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(32 * 32, 48, kernel_size = 3),
+            torch.nn.Conv2d(in_channels = 3, out_channels = 2, kernel_size = 5),
             torch.nn.ReLU()
         )
         self.decoder = torch.nn.Sequential(
-            torch.nn.Conv2d(48, 32 * 32, kernel_size = 3),
+            torch.nn.ConvTranspose1d(in_channels = 2, out_channels = 3, kernel_size = (5, 5)),
             torch.nn.Sigmoid()
         )
   
@@ -81,7 +84,7 @@ class AE(torch.nn.Module):
 model = AE()
   
 # Validation using MSE Loss function
-loss_function = torch.nn.MSELoss()
+loss_function = torch.nn.L1Loss()
   
 # Using an Adam Optimizer with lr = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3)
@@ -89,7 +92,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3)
 # DataLoader is used to load the dataset 
 # for training
 loader_1 = torch.utils.data.DataLoader(dataset = train_set,
-                                     batch_size = 5,
+                                     batch_size = BATCH_SIZE,
                                      shuffle = True)
 
 
@@ -99,11 +102,11 @@ outputs = []
 losses = []
 for epoch in range(epochs):
     for noisy_imgs_1, noisy_imgs_2 in loader_1:
-        print(noisy_imgs_1.shape)
-        print(noisy_imgs_2.shape)
+        #print(noisy_imgs_1.shape)
+        #print(noisy_imgs_2.shape)
 
-        noisy_imgs_1 = noisy_imgs_1.reshape(-1, 32 * 32)
-        noisy_imgs_2 = noisy_imgs_2.reshape(-1, 32 * 32)    
+        #noisy_imgs_1 = noisy_imgs_1.reshape(-1, 32 * 32)
+        #noisy_imgs_2 = noisy_imgs_2.reshape(-1, 32 * 32)    
         # Output of Autoencoder
         reconstructed = model(noisy_imgs_1)
             
@@ -117,7 +120,7 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         # Storing the losses in a list for plotting
-        losses.append(loss)
+        losses.append(loss.detach().numpy())
     outputs.append((epochs, noisy_imgs_2, reconstructed))
   
 # Defining the Plot Style
@@ -127,4 +130,5 @@ plt.ylabel('Loss')
   
 # Plotting the last 100 values
 plt.plot(losses[-100:])
+plt.show()
                         
