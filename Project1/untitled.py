bae02f27c094ebe5fc1c04dfe23cff3634fc2c5e
Louis Poulain--Auzéau <louis.poulain-auzeau@epfl.ch>
2022-04-22 17:19:06 +0200

    add file

---
+++ Project1/untitled.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class Noise2noise(nn.Module):
+    def __init__(self):
+        super(Noise2noise, self).__init__()
+        
+        # define layers and activation functions
+        
+    
+    
